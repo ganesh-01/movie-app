1ea8b37f0dfd84eda8a35ebdf8a1e090d631c9c5
ganeshbabu1 <ganesh.murali@cscs.io>
2024-03-27 17:30:12 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,81 @@
-import logo from './logo.svg';
-import './App.css';
+import SearchIcon from "./search-icon.svg";
+import { useEffect, useState } from "react";
+import "./App.css";
+import MovieCard from "./MovieCard";
 
-function App() {
+//SETTING UP API KEY FROM OMDB FREE API
+//http://www.omdbapi.com/?i=tt3896198&apikey=57b98dda
+const API_URL = "http://www.omdbapi.com/?apikey=57b98dda";
+
+const movie1 = {
+  Title: "Italian Spiderman",
+  Year: "2007",
+  imdbID: "tt2705436",
+  Type: "movie",
+  Poster: "N/A",
+};
+
+const App = () => {
+  const [movies, setMovies] = useState([]);
+  const [searchTerm, setSearchTerm] = useState("");
+  let resultText = `${movies.length} matches found`;
+  const [isClicked, setIsClicked] = useState(0);
+
+  const searchMovies = async (title) => {
+    const response = await fetch(`${API_URL}&s=${title}`);
+    const data = await response.json();
+    console.log(data.Search);
+    setMovies(data.Search);
+  };
+
+  useEffect(() => {
+    searchMovies("marvel");
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className="app">
+        <h1 className="headTitle">CINEMANIA</h1>
+
+        <div className="search">
+          <input
+            type="text"
+            placeholder="Search for movies"
+            value={searchTerm}
+            onChange={(e) => {
+              setSearchTerm(e.target.value);
+            }}
+          />
+          <img
+            src={SearchIcon}
+            alt="Searchicon"
+            onClick={() => {
+              searchMovies(searchTerm);
+              setIsClicked(1);
+              console.log(isClicked);
+            }}
+          />
+        </div>
+
+        <h3>{isClicked == 0 ? "" : resultText}</h3>
+        {movies?.length > 0 ? (
+          <div className="container">
+            {/* FOR SHOWING SINGLE MOVIE CARD */}
+            {/* <MovieCard movie1={movies[0]}/> */}
+
+            {/* FOR SHOWING ALL MOVIES IN ARRAY USING MAP */}
+            {movies.map((movie) => (
+              <MovieCard movie={movie} />
+            ))}
+          </div>
+        ) : (
+          <div className="empty">
+            <h2>No movies found</h2>
+          </div>
+        )}
+      </div>
+    </>
   );
-}
+};
 
 export default App;
